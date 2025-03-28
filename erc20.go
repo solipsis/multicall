4e856e4c266b9139d0ff515a9a5bf35af0089af8
dave <dave.alexander@hey.com>
2025-03-28 17:45:47 -0600

    vendor contracts and setup modules

--- erc20.go
+++ erc20.go
@@ -7,7 +7,7 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/gadzefinance/topup/src/contracts"
+	"github.com/solipsis/multicall/contracts"
 )
 
 // BalanceOf is a multicall batched call for any contract that implements the following solidity interface
